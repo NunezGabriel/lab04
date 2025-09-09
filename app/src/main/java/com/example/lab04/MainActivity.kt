7438a0adc8f0dc1eb5445d9a3dc0fe5ef4e9e8df
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-09-09 11:43:14 -0500

    add: berserk img

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,6 +47,15 @@ fun AgeInputScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.mark),
+            contentDescription = "Decorative Image",
+            modifier = Modifier
+                .size(200.dp) //
+                .padding(bottom = 16.dp)
+        )
+
         Text(
             text = "Ingresa tu edad",
             fontSize = 24.sp,
